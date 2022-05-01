b3290ccd7783545def0df9451189287a43f23929
rahad2694 <rahad.haw@gmail.com>
2022-05-02 00:59:37 +0600

    Mongodb added

--- index.js
+++ index.js
@@ -9,10 +9,37 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/',(req,res)=>{
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qqcx8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const collection = client.db("wmsDb").collection("products");
+        await client.connect();
+        console.log('DB Connected');
+
+    }
+    finally{
+        
+    }
+}
+run().catch(console.dir);
+
+// client.connect(err => {
+//     const collection = client.db("test").collection("devices");
+//     console.log('MongoDB Connected');
+//     // perform actions on the collection object
+//     client.close();
+// });
+
+
+app.get('/', (req, res) => {
     res.send('Server is running')
 })
 
-app.listen(port,()=>{
-    console.log('Listening to-',port);
+app.listen(port, () => {
+    console.log('Listening to-', port);
 })
